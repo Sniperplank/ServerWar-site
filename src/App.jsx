191a2b80c772a329b807aba68e4ecbb769bb0dce
Eilaf Aljundi <elafjundi@gmail.com>
2023-02-02 21:20:57 -0500

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import { Routes, Route, Link } from 'react-router-dom'
+import Commands from './pages/Commands'
+import Help from './pages/Help'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
+      <nav className='flex absolute left-0 right-0 justify-center items-center h-14 p-8'>
+        <Link className='px-3 py-2 text-xl text-gray-300 font-medium mx-8 hover:border-b-4 border-rose-600 hover:text-rose-600' to='/'>Home</Link>
+        <Link className='px-3 py-2 text-xl text-gray-300 font-medium mx-8 hover:border-b-4 border-rose-600 hover:text-rose-600' to='/commands'>Commands</Link>
+        <Link className='px-3 py-2 text-xl text-gray-300 font-medium mx-8 hover:border-b-4 border-rose-600 hover:text-rose-600' to='/help'>Help</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/commands' element={<Commands />} />
+        <Route path='/help' element={<Help />} />
       </Routes>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        navHover: {
+          '0%': { width: '0%' },
+          '100%': { width: '100%' },
+        },
+      },
+      animation: {
+        'nav-button-hover': 'navHover 1s linear forwards',
+      },
+    },
   },
   plugins: [],
 }
